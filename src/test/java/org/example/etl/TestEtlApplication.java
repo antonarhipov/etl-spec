4158bcd20b71fc8931733cf15e41606fe8f84f2b
antonarhipov <ant.arhipov@gmail.com>
2026-03-27 17:00:17 +0200

    initial state

--- src/test/java/org/example/etl/TestEtlApplication.java
+++ src/test/java/org/example/etl/TestEtlApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 public class TestEtlApplication {
 
     public static void main(String[] args) {
-        SpringApplication.from(EtlApplication::main).with(TestcontainersConfiguration.class).run(args);
+        SpringApplication.from(Application::main).with(TestcontainersConfiguration.class).run(args);
     }
 
 }
